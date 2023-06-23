0fb7cb883f8e1b27557fa4d5af82b7ed9216ae95
chloehjung <37277782+chloehjung@users.noreply.github.com>
2023-06-23 17:02:59 +1200

    Refactor authEv2First code into a function. Pass key number and key value as params

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -40,97 +40,19 @@ export default function TestScreen({ navigation }) {
             await NfcManager.requestTechnology(NfcTech.IsoDep);
             // the resolved tag object will contain `ndefMessage` property
 
-            //iso select file before auth
-            const isoSelectFileBytes = hexToBytes("00A4040007D276000085010100");
-            const isoSelectRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(isoSelectFileBytes) : await NfcManager.transceive(isoSelectFileBytes);
-            console.warn('isoSelectRes: ', Platform.OS == 'ios' ? bytesToHex([isoSelectRes.sw1, isoSelectRes.sw2]) : bytesToHex(isoSelectRes));
+            const {sesAuthMacKey, ti} = await AuthEv2First('00', '00000000000000000000000000000000');
+            const commandMac = CryptoJS.CMAC(CryptoJS.enc.Hex.parse(sesAuthMacKey), CryptoJS.enc.Hex.parse("F50000"+ti+'02'));
+            const commandMacHex = commandMac.toString();
+            console.log('commandMacHex', commandMacHex, hexToBytes(commandMacHex));
+            //truncate to 8 bytes (only get even numbered bytes)
+            const truncatedMacBytes = hexToBytes(commandMacHex).filter(function(element, index, array) {
+              return ((index + 1) % 2 === 0);
+            });
+            console.log('truncatedMac', truncatedMacBytes, bytesToHex(truncatedMacBytes));
 
-            const bytes = hexToBytes('9071000005000300000000');
-            console.log('bytes', bytes)
-            const Result = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(bytes) : await NfcManager.transceive(bytes);
-            console.warn('Result: ', Platform.OS == 'ios' ? bytesToHex([Result.sw1, Result.sw2]) : bytesToHex(Result));
-            const resultData = bytesToHex(Result.response);
-            console.log('resultData', resultData);
-            console.log('resultData', hexToBytes(resultData));
-            //91AF is the successful code
-            //@TODO: it might be different for android to get the result code
-            const resultCode = bytesToHex([Result.sw1, Result.sw2]);
-            if(resultCode == '91af') {
-              const key = CryptoJS.enc.Hex.parse("00000000000000000000000000000000");
-              const iv = CryptoJS.enc.Hex.parse("00000000000000000000000000000000");
-              const aesEncryptOption = {padding: CryptoJS.pad.NoPadding, mode: CryptoJS.mode.CBC, iv: iv, keySize: 128 / 8};
-              const RndBDec = AES.decrypt({ciphertext: CryptoJS.enc.Hex.parse(resultData)}, key, aesEncryptOption);
-              const RndB = CryptoJS.enc.Hex.stringify(RndBDec);
-              console.log('key',key, 'iv', iv);
-              console.log('rndb',RndB);
-              const RndABytes = randomBytes(16);
-              const RndA = bytesToHex(RndABytes);
-              console.log('rnda', bytesToHex(RndABytes));
-              const RndBRotlBytes = leftRotate(hexToBytes(RndB));
-              const RndBRotl = bytesToHex(RndBRotlBytes);
-              console.log('RndBRotl', RndBRotlBytes, RndBRotl);
-  
-              const RndARndBRotl = RndA + RndBRotl;
-              console.log('RndARndBRotl', RndARndBRotl);
-              const RndARndBEncData = AES.encrypt(CryptoJS.enc.Hex.parse(RndARndBRotl), key, aesEncryptOption);
-              const RndARndBEnc = RndARndBEncData.ciphertext.toString(CryptoJS.enc.Hex);
-              console.log('RndARndBEnc',RndARndBEnc);
-              console.log('RndARndBEnc',hexToBytes(RndARndBEnc));
-          
-              const secondAuthBytes = hexToBytes('90AF000020'+RndARndBEnc+'00');
-              console.log('90AF000020'+RndARndBEnc+'00');
-              console.log('secondAuthBytes', secondAuthBytes);
-              const secondAuthRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(secondAuthBytes) : await NfcManager.transceive(secondAuthBytes);
-              console.warn('Result: ', Platform.OS == 'ios' ? bytesToHex([secondAuthRes.sw1, secondAuthRes.sw2]) : bytesToHex(secondAuthRes));
-              //9100 is the successful code
-              //@TODO: it might be different for android to get the result code
-              const secondAuthResultCode = bytesToHex([secondAuthRes.sw1, secondAuthRes.sw2]);
-              if(secondAuthResultCode == '9100') {
-                //auth successful
-                const secondAuthResultData = bytesToHex(secondAuthRes.response);
-                const secondAuthResultDataDec = AES.decrypt({ciphertext: CryptoJS.enc.Hex.parse(secondAuthResultData)}, key, aesEncryptOption);
-                const secondAuthResultDataDecStr = CryptoJS.enc.Hex.stringify(secondAuthResultDataDec);
-                console.log('secondAuthResultDataDec', secondAuthResultDataDecStr);
-
-                const tiBytes = hexToBytes(secondAuthResultDataDecStr).slice(0,4);
-                const ti = bytesToHex(tiBytes);
-                console.log('ti', ti, tiBytes);
-
-                //SV2 = 5Ah||A5h||00h||01h||00h||80h||RndA[15..14]|| ( RndA[13..8] # RndB[15..10])||RndB[9..0]||RndA[7..0]
-                  //# == XOR-operator
-
-                console.log(RndA.slice(0,4), RndA.slice(4,16));
-                let sv2 = "5AA500010080";
-                sv2 = sv2 + RndA.slice(0,4);
-                
-                var WordArray = CryptoJS.lib.WordArray;
-
-                const xor =  CryptoJS.ext.xor(new WordArray.init(hexToBytes(RndA.slice(4, 16))), new WordArray.init(hexToBytes(RndB.slice(0, 12))));
-                sv2 = sv2 + bytesToHex(xor.words);
-                sv2 = sv2 + RndB.slice(12, 32) + RndA.slice(16, 32);
-                console.log('sv2', sv2);
-                const sesAuthMac = CryptoJS.CMAC(key, CryptoJS.enc.Hex.parse(sv2));
-                const sesAuthMacKey = sesAuthMac.toString();
-                console.log('sesAuthMacKey', sesAuthMacKey);
-
-                const commandMac = CryptoJS.CMAC(CryptoJS.enc.Hex.parse(sesAuthMacKey), CryptoJS.enc.Hex.parse("F50000"+ti+'02'));
-                const commandMacHex = commandMac.toString();
-                console.log('commandMacHex', commandMacHex, hexToBytes(commandMacHex));
-                //truncate to 8 bytes (only get even numbered bytes)
-                const truncatedMacBytes = hexToBytes(commandMacHex).filter(function(element, index, array) {
-                  return ((index + 1) % 2 === 0);
-                });
-                console.log('truncatedMac', truncatedMacBytes, bytesToHex(truncatedMacBytes));
-
-                const getFileSettingsHex = "90F500000902" + bytesToHex(truncatedMacBytes) + '00';
-                const getFileSettingsRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(hexToBytes(getFileSettingsHex)) : await NfcManager.transceive(hexToBytes(getFileSettingsHex));
-                console.warn('getFileSettingsRes Result: ', Platform.OS == 'ios' ? bytesToHex([getFileSettingsRes.sw1, getFileSettingsRes.sw2]) : bytesToHex(getFileSettingsRes));
-              } else {
-                //auth failed
-              }
-            } else {
-              //auth failed
-            }
+            const getFileSettingsHex = "90F500000902" + bytesToHex(truncatedMacBytes) + '00';
+            const getFileSettingsRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(hexToBytes(getFileSettingsHex)) : await NfcManager.transceive(hexToBytes(getFileSettingsHex));
+            console.warn('getFileSettingsRes Result: ', Platform.OS == 'ios' ? bytesToHex([getFileSettingsRes.sw1, getFileSettingsRes.sw2]) : bytesToHex(getFileSettingsRes));
 
           } catch (ex) {
           console.warn('Oops!', ex);
@@ -139,6 +61,96 @@ export default function TestScreen({ navigation }) {
           NfcManager.cancelTechnologyRequest();
         }
       }
+
+    /*
+    * keyNo String (00, 01, 02)
+    * key String (hex value)
+    */
+    const AuthEv2First = async (keyNo, pKey) => {
+      //iso select file before auth
+      const isoSelectFileBytes = hexToBytes("00A4040007D276000085010100");
+      const isoSelectRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(isoSelectFileBytes) : await NfcManager.transceive(isoSelectFileBytes);
+      console.warn('isoSelectRes: ', Platform.OS == 'ios' ? bytesToHex([isoSelectRes.sw1, isoSelectRes.sw2]) : bytesToHex(isoSelectRes));
+
+      const bytes = hexToBytes('9071000005'+keyNo+'0300000000');
+      console.log('bytes', bytes)
+      const Result = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(bytes) : await NfcManager.transceive(bytes);
+      console.warn('Result: ', Platform.OS == 'ios' ? bytesToHex([Result.sw1, Result.sw2]) : bytesToHex(Result));
+      const resultData = bytesToHex(Result.response);
+      console.log('resultData', resultData);
+      console.log('resultData', hexToBytes(resultData));
+      //91AF is the successful code
+      //@TODO: it might be different for android to get the result code
+      const resultCode = bytesToHex([Result.sw1, Result.sw2]);
+      if(resultCode == '91af') {
+        const key = CryptoJS.enc.Hex.parse(pKey);
+        const iv = CryptoJS.enc.Hex.parse("00000000000000000000000000000000");
+        const aesEncryptOption = {padding: CryptoJS.pad.NoPadding, mode: CryptoJS.mode.CBC, iv: iv, keySize: 128 / 8};
+        const RndBDec = AES.decrypt({ciphertext: CryptoJS.enc.Hex.parse(resultData)}, key, aesEncryptOption);
+        const RndB = CryptoJS.enc.Hex.stringify(RndBDec);
+        console.log('key',key, 'iv', iv);
+        console.log('rndb',RndB);
+        const RndABytes = randomBytes(16);
+        const RndA = bytesToHex(RndABytes);
+        console.log('rnda', bytesToHex(RndABytes));
+        const RndBRotlBytes = leftRotate(hexToBytes(RndB));
+        const RndBRotl = bytesToHex(RndBRotlBytes);
+        console.log('RndBRotl', RndBRotlBytes, RndBRotl);
+
+        const RndARndBRotl = RndA + RndBRotl;
+        console.log('RndARndBRotl', RndARndBRotl);
+        const RndARndBEncData = AES.encrypt(CryptoJS.enc.Hex.parse(RndARndBRotl), key, aesEncryptOption);
+        const RndARndBEnc = RndARndBEncData.ciphertext.toString(CryptoJS.enc.Hex);
+        console.log('RndARndBEnc',RndARndBEnc);
+        console.log('RndARndBEnc',hexToBytes(RndARndBEnc));
+    
+        const secondAuthBytes = hexToBytes('90AF000020'+RndARndBEnc+'00');
+        console.log('90AF000020'+RndARndBEnc+'00');
+        console.log('secondAuthBytes', secondAuthBytes);
+        const secondAuthRes = Platform.OS == 'ios' ? await NfcManager.sendCommandAPDUIOS(secondAuthBytes) : await NfcManager.transceive(secondAuthBytes);
+        console.warn('Result: ', Platform.OS == 'ios' ? bytesToHex([secondAuthRes.sw1, secondAuthRes.sw2]) : bytesToHex(secondAuthRes));
+        //9100 is the successful code
+        //@TODO: it might be different for android to get the result code
+        const secondAuthResultCode = bytesToHex([secondAuthRes.sw1, secondAuthRes.sw2]);
+        if(secondAuthResultCode == '9100') {
+          //auth successful
+          const secondAuthResultData = bytesToHex(secondAuthRes.response);
+          const secondAuthResultDataDec = AES.decrypt({ciphertext: CryptoJS.enc.Hex.parse(secondAuthResultData)}, key, aesEncryptOption);
+          const secondAuthResultDataDecStr = CryptoJS.enc.Hex.stringify(secondAuthResultDataDec);
+          console.log('secondAuthResultDataDec', secondAuthResultDataDecStr);
+
+          const tiBytes = hexToBytes(secondAuthResultDataDecStr).slice(0,4);
+          const ti = bytesToHex(tiBytes);
+          console.log('ti', ti, tiBytes);
+
+          //SV2 = 5Ah||A5h||00h||01h||00h||80h||RndA[15..14]|| ( RndA[13..8] # RndB[15..10])||RndB[9..0]||RndA[7..0]
+            //# == XOR-operator
+
+          console.log(RndA.slice(0,4), RndA.slice(4,16));
+          let sv2 = "5AA500010080";
+          sv2 = sv2 + RndA.slice(0,4);
+          
+          var WordArray = CryptoJS.lib.WordArray;
+
+          const xor =  CryptoJS.ext.xor(new WordArray.init(hexToBytes(RndA.slice(4, 16))), new WordArray.init(hexToBytes(RndB.slice(0, 12))));
+          sv2 = sv2 + bytesToHex(xor.words);
+          sv2 = sv2 + RndB.slice(12, 32) + RndA.slice(16, 32);
+          console.log('sv2', sv2);
+          const sesAuthMac = CryptoJS.CMAC(key, CryptoJS.enc.Hex.parse(sv2));
+          const sesAuthMacKey = sesAuthMac.toString();
+          console.log('sesAuthMacKey', sesAuthMacKey);
+
+          return Promise.resolve({sesAuthMacKey, ti});
+
+        } else {
+          //auth failed
+          return Promise.reject("Auth Failed: "+secondAuthResultCode)
+        }
+      } else {
+        //auth failed
+        return Promise.reject("Auth Failed: "+resultCode)
+      }
+    }
     return (
         <ScrollView>
         <Card style={{ marginBottom: 20, marginHorizontal: 10 }}>
